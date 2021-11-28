5fc2141fceb2f92aa3460705c31b2bd52e08488d
Hamilkar247 <adonnibal96@gmail.com>
2021-11-29 00:21:06 +0100

    [WO-1] dodawannja statycznych imen  - ajout de noms statiques
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//         випадкові імена / wylosuj imiona
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//         </a>
+//       </header>
+
+//     </div>
+//   );
+// }
+
+function Dobroho_ranku(props) {
+  return <h5>доброго ранку/Bonjour, {props.name}</h5>;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="czolowiczi imena">
+          <h5> чоловічі імена / noms masculins </h5>
+          <Dobroho_ranku name="Andriy" />
+          <Dobroho_ranku name="Artem" />
+          <Dobroho_ranku name="Antin" />
+        </div>
+        <div className="żinoczi imena">
+          <h5> жіночі імена / noms féminins </h5>
+          <Dobroho_ranku name="Anna"/>
+          <Dobroho_ranku name="Bohdana"/>
+          <Dobroho_ranku name="Myroslava"/>
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
